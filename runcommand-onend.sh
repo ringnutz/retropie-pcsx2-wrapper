4caca3ffd1277bb18006e8a06dad8603105181b0
Adam Ringer <ringnutz@gmail.com>
2019-12-31 10:37:43 -0500

    fixed spaces

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #only if for ps2, kill python wrapper script
-if [ $1='ps2' ]; then
+if [ $1 = 'ps2' ]; then
 	PROCS=$(ps aux | grep gamepad_wrapper.py | awk '{ print $2 }')
 	for PROC in ${PROCS[@]}
 	do
